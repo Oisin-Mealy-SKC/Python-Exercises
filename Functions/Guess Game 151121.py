da0cdf65b3703d989c84961c3a6f8be2154e1139
Oisin-Mealy-SKC <96420962+Oisin-Mealy-SKC@users.noreply.github.com>
2022-01-12 10:28:56 +0000

    Add files via upload

---
+++ Functions/Guess Game 151121.py
@@ -0,0 +1,23 @@
+#Guess Game - 3 guesses
+import random
+
+number = random.randint(1,10)
+print (number)
+
+counter = 0
+
+while counter < 3:
+
+    guess = int(input("Enter a number between 1 and 10: "))
+    if guess == number:
+        print ("Correct")
+        break
+    elif guess < number:
+        print ("Too low")
+    else:
+        print ("Too high")
+        
+    counter = counter + 1
+    
+print ("Goodbye")
+            
---
+++ Functions/While Functions  1511121.py
@@ -0,0 +1,7 @@
+number = 1
+
+while number < 1000:
+    print ("The number is less than 1000")
+    number = number*2
+print ("Number is now above 1000")
+    
