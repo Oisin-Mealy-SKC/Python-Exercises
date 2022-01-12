c24bfb99bf5dc7a42017e6cbdbf8b01ff1bba854
Oisin-Mealy-SKC <96420962+Oisin-Mealy-SKC@users.noreply.github.com>
2022-01-12 10:25:59 +0000

    Add files via upload

---
+++ Week 1 End/Class Notes 080921.py
@@ -0,0 +1,4 @@
+print("Python is a porgramming language to get a computer to coplete a task")
+print("Thony is the IDE we are using")
+print("Python has a syntax which is a set of rules it must follow")
+print("Python has 33 key words or commands which are specially reserved words to complete a task")
